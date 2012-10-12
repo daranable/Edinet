adf749c0d5e2ef19520bd138f7abad91a023bccc
robert <daranable@gmail.com>
2012-10-12 00:57:12 -0500

    Added get and set methods to Course.java

--- src/main/java/net/maltera/daranable/edinet/model/Course.java
+++ src/main/java/net/maltera/daranable/edinet/model/Course.java
@@ -1,5 +1,6 @@
 package net.maltera.daranable.edinet.model;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.sql.Clob;
 import java.sql.ResultSet;
@@ -11,6 +12,10 @@ public class Course {
 	private final int id;
 	private String teacher, notes, name; 
 	private int year, term;
+	private boolean teacher_dirty, notes_dirty, name_dirty; 
+	private boolean year_dirty, term_dirty, color_dirty;
+	private Color color;
+	private Repository repo;
 	
 	private Course( int id ) {
 		this.id = id;
@@ -30,7 +35,55 @@ public class Course {
 		inst.notes = CharStreams.toString( notes.getCharacterStream() );
 		notes.free();
 		
+		int colorInt = result.getInt( "color" );
+		inst.color = new Color( colorInt );
+		
+		inst.repo = repo;
+		
 		return inst;
 	}
-
+	
+	public String getTeacher() {
+		return teacher;
+	}
+	
+	public void setTeacher( String teacher ) {
+		if ( this.teacher == teacher ) return;
+		
+		this.teacher = teacher;
+		this.teacher_dirty = true;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public void setNotes( String notes ) {
+		if ( this.notes == notes ) return;
+		
+		this.notes = notes;
+		this.notes_dirty = true;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName( String name ) {
+		if ( this.name == name ) return;
+		
+		this.name = name;
+		this.name_dirty = true;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public void setColor( Color color ) {
+		if ( this.color == color ) return;
+		
+		this.color = color;
+		this.color_dirty = true;
+	}
 }
