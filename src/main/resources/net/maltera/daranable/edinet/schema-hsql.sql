296ca5ff6d92fab62932c1f060d3200b884ef82f
robert <daranable@gmail.com>
2012-10-12 01:52:02 -0500

    Added properties table to database Schema, and set version number to 0.

--- src/main/resources/net/maltera/daranable/edinet/schema-hsql.sql
+++ src/main/resources/net/maltera/daranable/edinet/schema-hsql.sql
@@ -1,3 +1,10 @@
+CREATE TABLE properties (
+	name VARCHAR (128) NOT NULL IDENTITY PRIMARY KEY,
+	value VARCHAR (256)
+);
+
+INSERT INTO properties (key, value) VALUES ('schema.version', '0');
+
 CREATE TABLE terms (
 	year SMALLINT NOT NULL,
 	serial SMALLINT NOT NULL,
@@ -9,7 +16,7 @@ CREATE TABLE terms (
 )
 
 CREATE TABLE courses (
-	id INTEGER NOT NULL IDENTITY,
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
 	year SMALLINT NOT NULL,
 	term SMALLINT NOT NULL,
 	teacher VARCHAR (256),
@@ -24,7 +31,7 @@ CREATE TABLE courses (
 )
 
 CREATE TABLE assignments (
-	id INTEGER NOT NULL IDENTITY,
+	id INTEGER NOT NULL IDENTITY PRIMARY KEY,
 	course_id INTEGER NOT NULL,
 	title VARCHAR (256),
 	description CLOB,
