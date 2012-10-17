38c1a2580b304a390f08b5df6024cedf1af02ba5
robert <daranable@gmail.com>
2012-10-17 06:13:48 -0500

    Added JUnit test for testing updating Assignments.

--- src/test/java/net/maltera/daranable/edinet/TestAssignments.java
+++ src/test/java/net/maltera/daranable/edinet/TestAssignments.java
@@ -98,4 +98,73 @@ public class TestAssignments {
 		
 		database.shutdown();
 	}
+	
+	@Test
+	public void testEditAssignment() 
+	throws Exception {
+		Repository repo = new Repository();
+		Database database = repo.getDatabase();
+		Date startDate = new Date();
+		
+		// Create the new Term to use with my course.
+		Term term = Term.create( repo );
+		
+		// Set it's default values
+		term.setYear( 2012 );
+		term.setName( "Fall" );
+		term.setStartDate( startDate );
+		
+		// Push its values to the db
+		term.commit();
+		
+		Color color = new Color( 0, 255, 0 );
+		
+		// Create a new course to put the assignment for.
+		Course course = Course.create( repo );
+		
+		course.setTermReference( term );
+		course.setTeacher( "Ceto" );
+		course.setName( "Underwater Basket Weaving 101" );
+		course.setAbbreviation( "UBW-101" );
+		course.setColor( color );
+		course.setNotes( "Basic information on making baskets under water." );
+		
+		course.commit();
+		
+		Assignment assign = Assignment.create( repo );
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set( 2012, 10, 31 );
+		
+		assign.setCourse( course );
+		assign.setTitle( "Read Chapter 1: Know your Seaweed" );
+		assign.setTimeEstimate( 60 );
+		
+		assign.commit();
+		
+		int assign_id = assign.getId();
+		
+		assign.setTitle( "Read Chapter 2: Strengthening your Seaweed" );
+		assign.setDescription( "Chapter covers going over different weaves " +
+				"and much more." );
+		
+		assign.commit();
+		
+		assign = null;
+		
+		assign = repo.getAssignment( assign_id );
+		
+		assertTrue( "course is not the same", 
+				assign.getCourse().equals( course ) );
+		assertEquals( "title", 
+				"Read Chapter 2: Strengthening your Seaweed", 
+				assign.getTitle() );
+		assertTrue( "time estimate is not the same", 
+				assign.getTimeEstimate() == 60 );
+		assertEquals( "Chapter covers going over different weaves " +
+				"and much more.",
+				assign.getDescription() );
+		
+		database.shutdown();
+	}
 }
